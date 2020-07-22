603a9453feaafe85faf0aa201390d0ae1419eedd
Cleilson <marcialwushu@gmail.com>
2020-07-22 05:22:05 +0000

    routes for componets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{ HomeComponent} from './components/home/home.component';
+import{ FaqComponent } from './components/faq/faq.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'',
+  component: HomeComponent
+},
+{
+  path:'faq',
+  component: FaqComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
